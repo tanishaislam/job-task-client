421706bd1d139acd47514b1c15cf548767bc4285
tanishaislam <dev.eva.2006@gmail.com>
2023-12-22 18:35:12 +0600

    about page design

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,7 +3,22 @@
 const About = () => {
     return (
         <div>
-            this is about page
+            <div>
+            <div className=" " style={{backgroundImage: 'url(https://i.ibb.co/pjnLgr1/mdr2.jpg)'}}>
+           <div className="container mx-auto ">
+           <div className="flex lg:flex-row flex-col justify-around items-center gap-9 lg:h-[100vh] h-auto lg:pt-2 pt-20">
+                <div className='bg-black lg:p-20 md:p-16 p-5 bg-opacity-50 mb-7 mt-3'>
+                <h1 className="lg:text-3xl md:text-2xl text-xl font-bold text-center text-white" >About page</h1>
+                <div className="border-b-4 w-28 border-blue-600 my-5 m-auto "></div>
+                <div className="flex justify-center items-center flex-col">
+                   <p className="text-white w-1/2 mt-4 text-center"> Debitis nobis voluptate minus quae quasi optio molestiae soluta sunt facere provident! Voluptates ipsum consequatur nesciunt perspiciatis repudiandae quia eveniet ipsa, reiciendis aut, ut quas? Maiores quidem autem, deleniti similique illo ab.</p>  
+                   <button className=" py-2 bg-white text-blue-500 px-6 rounded-sm font-bold mt-8">See More Details</button>       
+                </div>
+                </div>
+           </div>
+           </div>
+        </div>
+        </div>
         </div>
     );
 };
